6eb20305cc45396ddf53e457149d56f881028ea6
LuizD13 <luizclaudio_azevedo@hotmail.com>
2020-02-05 20:37:34 -0300

    Re-início dos cursos de Python

---
+++ AulasPython/PythonBasico/aula4.py
@@ -0,0 +1,12 @@
+"""
+Tipos de dados do Python:
+str - String
+int - Inteiro
+float - real (double)
+bool - True ou false (lógico)
+
+"""
+print(1, type(1))  # Type - imprime o tipo de dado do que está sendo impresso
+print('Luiz', type('Luiz'))
+print(25.23, type(25.23))
+print(10 == 10, type(10 == 10))
