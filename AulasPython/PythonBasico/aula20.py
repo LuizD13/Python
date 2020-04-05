dc77d118c034d4e91aa063d7ede824ed1a4ad529
LuizD13 <37920681+LuizD13@users.noreply.github.com>
2020-04-05 19:23:48 -0300

    Aulas 20 e 21

---
+++ AulasPython/PythonBasico/aula20.py
@@ -0,0 +1,21 @@
+"""
+For in - Estrutura de repetição em Python
+Iterando Strings com For
+função range (start=0,stop,step=1)
+"""
+
+texto = 'Python basico'
+nova_string = ''
+
+for letra in texto:
+    if letra == 't' or letra == 'h':
+        continue  # ele pula para a próxima repetição
+        nova_string = nova_string + letra.upper()
+    else:
+        nova_string += letra
+        break  # interrompe a repetição e termina o For
+    # print(letra.strip())  # strip - remove espaços da string
+print(nova_string)
+
+for numero in range(11, 132, 11):  # múltiplos de 11 até o próprio 11
+    print(numero)
---
+++ AulasPython/PythonBasico/aula21.py
@@ -0,0 +1,14 @@
+"""
+Listas em Python
+fatiamento
+append, insert, pop, del, clear, extend, +
+min, max
+range
+"""
+texto = 'Valor '
+
+# Indice: 0    1    2    3    4
+lista = ['A', 'B', 'C', 'D', 'E']
+# '' -  : 5    4    3    2    1
+
+print(lista[-1])
